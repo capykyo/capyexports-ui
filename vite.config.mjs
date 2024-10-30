0c7aaa691ea53a912ba11691d56777331b3ffa16
kyoapps-kyo <kyo@kyoapps.com>
2024-10-30 23:53:46 +0800

    修改为测试版本号

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      entryRoot: "src", // 类型声明的入口
+      outDir: "dist/types", // 输出目录
+      insertTypesEntry: true, // 自动生成 types 字段入口
+    }),
+  ],
   build: {
     lib: {
       entry: "src/index.ts",
